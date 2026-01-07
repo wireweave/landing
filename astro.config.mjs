0dfbe8916201f4e92a088989b2d0ffcb653b8ab2
Seungwoo321 <seungwoo321@gmail.com>
2026-01-07 20:39:46 +0900

    Initial commit: Astro landing page

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://wireweave.dev',
+  integrations: [tailwind()],
+});
